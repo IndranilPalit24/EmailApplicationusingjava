c72a0d182528bd04b4fe9c5c4ba85f38aa5bc67a
Indranil_Palit <73847100+IndranilPalit24@users.noreply.github.com>
2022-03-16 10:52:50 +0530

    Add files via upload

---
+++ Program.java
@@ -0,0 +1,9 @@
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		  Email em1 = new Email("Person1", "Person2");
+		  System.out.println(em1.Show_Info());
+	}
+}
